b6adac327926817ca4ff6d60103a586f9e805320
shogoisaji <simacsimac9@gmail.com>
2024-11-04 18:48:14 +0900

    update: url

--- api/src/middleware/cors.ts
+++ api/src/middleware/cors.ts
@@ -1,7 +1,7 @@
 import { cors } from "hono/cors";
 
 export const corsMiddleware = cors({
-  origin: "https://e615a2dc.text-app-eai.pages.dev",
+  origin: "https://text-app-eai.pages.dev/",
   // origin: "http://localhost:5173",
   allowMethods: ["GET", "POST"],
 });
